26c5354515c570a9d852eca2930d1af057cb78bf
berry1c3cream-cell <berry1c3cream@gmail.com>
2026-06-01 20:22:54 -0600

    Se arregló la pagina principal y se agregó un script global para impedir errores de null

---
+++ PIA interfaz/global.js
@@ -0,0 +1,61 @@
+// Panel de accesibilidad
+const accBtn = document.getElementById("settings-btn");
+const accessPanel = document.getElementById("accessPanel");
+
+// Solo activa los eventos si AMBOS elementos existen en el HTML actual
+if (accBtn && accessPanel) {
+    accBtn.addEventListener("click", () => {
+        accessPanel.classList.toggle("show");
+
+        /* mover foco al primer input */
+        if (accessPanel.classList.contains("show")) {
+            const firstControl = accessPanel.querySelector("select");
+            firstControl?.focus();
+        }
+
+        const expanded = accessPanel.classList.contains("show");
+        accBtn.setAttribute("aria-expanded", expanded);
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+            accessPanel.classList.remove("show");
+            accBtn.focus();
+        }
+    });
+}
+
+// Toggle Letra Grande
+const fontSizeSelect = document.getElementById("font-size");
+
+if (fontSizeSelect) {
+
+    const savedSize =
+        localStorage.getItem("fontSize") || "normal";
+
+    fontSizeSelect.value = savedSize;
+
+    document.body.classList.add(savedSize);
+
+    fontSizeSelect.addEventListener("change", () => {
+
+        document.body.classList.remove(
+            "normal",
+            "large",
+            "xlarge"
+        );
+
+        document.body.classList.add(
+            fontSizeSelect.value
+        );
+
+        localStorage.setItem(
+            "fontSize",
+            fontSizeSelect.value
+        );
+
+    });
+}
+
+console.log(accBtn);
+console.log(accessPanel);
